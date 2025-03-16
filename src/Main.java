78b2868fb4fe061bb0f8c1bb9d7fc1d03a57df1b
Denis <dessitnov22@yandex.ru>
2025-03-16 13:38:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int x = 1; x <= 10; x++) {
+            System.out.println("тут можно писать все что угодно " + x);
+        }
+
+        System.out.println("Какието изменения");
+
+    }
+
+}
